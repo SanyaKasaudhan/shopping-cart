fe5b61a2f080b784bfbb1042b37f6cac7cf301a6
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-07-13 20:59:21 +0530

    update npm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,62 @@
+import { Search } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
 height:60px;
-background-color:black;
+background-color:yellow;
 `
+
+const Wrapper = styled.div`
+padding:10px 20px;
+display:flex;
+justify-content:space-between;
+`
+
+const Left=styled.div`
+  width:33.3%;
+  display:flex;
+  align-items:center;
+`
+const Language=styled.div`
+  font-size:14px;
+  cursor:pointer;
+
+`
+const SearchContainer=styled.div`
+ border:1px solid lightgray;
+`
+
+const Right=styled.div`
+width:33.3%;
+`
+
+const Center=styled.div`
+width:33.3%;
+`
+
 const Navbar = () => {
   return (
     <Container>
-        navbar
-        
+      <Wrapper>
+        <Left>
+          <Language>
+            <SearchContainer>
+            <Search />
+            input
+            </SearchContainer>
+            en</Language>
+         
+          </Left>
+        <Center>
+          Center
+        </Center>
+        <Right>
+          Right
+        </Right>
+      </Wrapper>
+      
+
 
 
     </Container>
